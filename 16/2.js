71546f24dc55c3bec45ce5146491c16809350056
anguslubos <angus.lubos@gmail.com>
2021-11-15 21:56:45 +0100

    day 16 - second part - not proud of solution, but it's working

--- 16/2.js
+++ 16/2.js
@@ -1,27 +1,72 @@
 import { AdventOfCode as BaseAdventOfCode } from '../AdventOfCode.js'
 
+Array.prototype.remove = function() {
+  var what, a = arguments, L = a.length, ax;
+  while (L && this.length) {
+      what = a[--L];
+      while ((ax = this.indexOf(what)) !== -1) {
+          this.splice(ax, 1);
+      }
+  }
+  return this;
+};
+
 class AdventOfCode extends BaseAdventOfCode
 {
+  get nearbyTickets() {
+    const nearbyTickets = /nearby tickets:\n(.*)/s.exec(this.inputData)[1].split('\n').map(v => v.split(',').map(Number))
+    const allValidValues = Object.values(this.validFieldsValues).reduce((accumulator, current) => [...accumulator, ...current], [])
+    const ticketsToRemove = []
+
+    for (let ticketIndex = 0; ticketIndex < nearbyTickets.length; ticketIndex++) {
+      const ticket = nearbyTickets[ticketIndex]
+
+      for (const value of ticket) {
+        if (!allValidValues.includes(value)) {
+          ticketsToRemove.push(ticketIndex)
+        }
+      }
+    }
+
+    for (const removeIndex of ticketsToRemove.reverse()) {
+      nearbyTickets.splice(removeIndex, 1)
+    }
+
+    return nearbyTickets
+  }
+
+  get myTicket() {
+    return /your ticket:\n(.*)/.exec(this.inputData)[1].split('\n').map(v => v.split(',').map(Number))[0]
+  }
+
+  get allTickets() {
+    return [...[this.myTicket], ...this.nearbyTickets]
+  }
+
   constructor (inputFileName) {
     super(inputFileName)
 
-    this.inputData = null
+    this.validFieldsValues = []
+    this.fieldsValues = []
   }
 
   parseInput(data) {
     this.inputData = data
+
+    this.validFieldsValues = this.getValidValues()
+    this.fieldsValues = this.getFieldsValues()
   }
 
-  get validValues() {
-    const regex = /(\w+): (.+) or (.+)/gm;
-    let fields = {};
-    let m;
+  getValidValues() {
+    const regex = /(.*): (.+) or (.+)/gm
+    let fields = {}
+    let m
 
     while ((m = regex.exec(this.inputData)) !== null) {
       if (m.index === regex.lastIndex) regex.lastIndex++
 
       let field = null
-      let validValues = []
+      let validFieldsValues = []
 
       m.forEach((match, groupIndex) => {
         if (groupIndex == 0) {}
@@ -29,72 +74,91 @@ class AdventOfCode extends BaseAdventOfCode
         else {
           const range = match.split('-')
           for (let index = parseInt(range[0]); index <= parseInt(range[1]); index++) {
-            validValues.push(index)
+            validFieldsValues.push(index)
           }
         }
       })
 
-      fields[field] = validValues
+      fields[field] = validFieldsValues
     }
 
     return fields
   }
 
-  get nearbyTickets() {
-    const nearbyTickets = /nearby tickets:\n(.*)/s.exec(this.inputData)[1].split('\n').map(v => v.split(',').map(Number))
-    const allValidValues = Object.values(this.validValues).reduce((accumulator, current) => [...accumulator, ...current], [])
+  getFieldsValues() {
+    const values = []
 
-    for (let ticketIndex = 0; ticketIndex < nearbyTickets.length; ticketIndex++) {
-      const ticket = nearbyTickets[ticketIndex]
+    for (const ticket of this.nearbyTickets) {
+      for (let index = 0; index < ticket.length; index++) {
+        if (!values[index]) {
+          values[index] = []
+        }
 
-      for (const value of ticket) {
-        if (!allValidValues.includes(value)) {
-          nearbyTickets.splice(ticketIndex, 1)
+        values[index].push(ticket[index])
+      }
+    }
+
+    return values
+  }
+
+  callback() {
+    const workingValues = new Array(this.fieldsValues.length)
+    const finalValues = new Array(this.fieldsValues.length)
+
+    for (let index = 0; index < workingValues.length; index++) {
+      workingValues[index] = Object.keys(this.validFieldsValues)
+    }
+
+    for (let index = 0; index < this.fieldsValues.length; index++) {
+      const fieldValues = this.fieldsValues[index];
+
+      for (const fieldValue of fieldValues) {
+        for (const possibleField of Object.keys(this.validFieldsValues)) {
+          if (!this.validFieldsValues[possibleField].includes(fieldValue)) {
+            workingValues[index].remove(possibleField)
+          }
         }
       }
     }
 
-    return nearbyTickets
-  }
+    for (let index = 0; index < workingValues.length; index++) {
+      const values = workingValues[index]
 
-  get myTicket() {
-    return /your ticket:\n(.*)/.exec(this.inputData)[1].split('\n').map(v => v.split(',').map(Number))
-  }
+      if (values.length === 0) {
+        continue
+      }
 
-  get allTickets() {
-    return [...this.myTicket, ...this.nearbyTickets]
-  }
+      if (values.length === 1) {
+        const fieldName = values[0]
+        finalValues[index] = fieldName
 
-  get correctFields() {
-    const correctFields = []
-
-    for (let ticketIndex = 0; ticketIndex < this.allTickets.length; ticketIndex++) {
-      const ticket = this.allTickets[ticketIndex]
-
-      console.log(ticket)
+        for (let j = 0; j < workingValues.length; j++) {
+          workingValues[j] = workingValues[j].remove(fieldName)
+        }
+        index = -1
+      }
     }
 
-    return correctFields
-  }
+    const departureIndexes = finalValues.reduce((acc, curr, index) => {
+      if (curr.includes('departure')) {
+        acc.push(index)
+      }
 
-  callback() {
-    console.table(this.correctFields)
+      return acc
+    }, [])
 
-    // const allValidValues = Object.values(this.fields).reduce((accumulator, current) => {
-    //   return [...accumulator, ...current]
-    // }, [])
+    return this.myTicket.reduce((acc, curr, index) => {
+      if (departureIndexes.includes(index)) {
+        if (acc === 0) {
+          acc = curr
+        } else {
+          acc = curr * acc
+        }
+      }
 
-    // for (const ticket of this.nearbyTickets) {
-    //   for (const value of ticket) {
-    //     if (!allValidValues.includes(value)) {
-    //       this.invalidValues.push(value)
-    //     }
-    //   }
-    // }
-
-    return 'lel'
-    // return eval(this.invalidValues.join('+'))
+      return acc
+    }, 0)
   }
 }
 
-new AdventOfCode('demo2').run()
+new AdventOfCode('input').run()

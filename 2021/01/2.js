b0a6bf70ab470d563c9a46b28032d474c4b42453
Ľuboško Anguš <lubos.angus@riesenia.com>
2021-12-01 16:31:26 +0100

    2021 - day 1 - part 2

---
+++ 2021/01/2.js
@@ -0,0 +1,31 @@
+import { AdventOfCode as BaseAdventOfCode } from '../../AdventOfCode.js'
+
+class AdventOfCode extends BaseAdventOfCode
+{
+  constructor (inputFileName) {
+    super(inputFileName)
+
+    this.inputAsInt = true
+  }
+
+  callback() {
+    let largerMeasurements = 0
+    let last = null
+
+    for (let index = 2; index < this.input.length; index++) {
+      const current = (new Array(3).fill()).reduce((acc, _, arrIndex) => {
+        return acc + this.input[index - arrIndex]
+      }, 0)
+
+      if (last !== null && current > last) {
+        largerMeasurements++
+      }
+
+      last = current
+    }
+
+    return largerMeasurements
+  }
+}
+
+new AdventOfCode('input').run()

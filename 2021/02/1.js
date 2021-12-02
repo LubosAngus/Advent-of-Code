54234030dae4f2f3ce88d3b8bc685723e7411e61
anguslubos <angus.lubos@gmail.com>
2021-12-03 00:30:55 +0100

    2021 day 2 part 1

--- 2021/02/1.js
+++ 2021/02/1.js
@@ -7,6 +7,32 @@ class AdventOfCode extends BaseAdventOfCode
   }
 
   callback() {
+    const position = {
+      horizontal: 0,
+      depth: 0
+    }
+
+    for (const row of this.input) {
+      let { instruction, value } = /(?<instruction>.*?) (?<value>\d+)/.exec(row).groups
+
+      value = parseInt(value)
+
+      switch (instruction) {
+        case 'forward':
+          position.horizontal += value
+          break;
+
+        case 'down':
+          position.depth += value
+          break;
+
+        case 'up':
+          position.depth -= value
+          break;
+      }
+    }
+
+    return position.horizontal * position.depth
   }
 }
 

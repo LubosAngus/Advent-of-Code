472a823888cefd077b1af8ac57f26a8f1d387e03
Ľuboško Anguš <lubos.angus@riesenia.com>
2022-07-12 15:20:26 +0200

    2021 - day 7 - part 1

---
+++ 2021/07/1.js
@@ -0,0 +1,46 @@
+import { AdventOfCode as BaseAdventOfCode } from '../../AdventOfCode.js'
+
+class AdventOfCode extends BaseAdventOfCode
+{
+  constructor(inputFileName) {
+    super(inputFileName)
+  }
+
+  parseInput(data) {
+    return data.split(',').map(Number)
+  }
+
+  callback() {
+    const potentialPositions = this.input.reduce((acc, item) => {
+      if (!acc.includes(item)) {
+        acc.push(item)
+      }
+
+      return acc
+    }, [])
+
+    let cheapestFuel = Infinity
+    let cheapestPosition = null
+
+    for (const position of potentialPositions) {
+      let fuel = 0
+
+      for (const crab of this.input) {
+        fuel = fuel + Math.abs(position - crab)
+
+        if (cheapestFuel < fuel) {
+          break;
+        }
+      }
+
+      if (cheapestFuel > fuel) {
+        cheapestFuel = fuel
+        cheapestPosition = position
+      }
+    }
+
+    return cheapestFuel
+  }
+}
+
+new AdventOfCode('input').run()

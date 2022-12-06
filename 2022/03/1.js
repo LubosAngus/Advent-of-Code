d8ebdea9741d8f197d4a7b11b6e65164b837c016
Ľuboško Anguš <lubos.angus@riesenia.com>
2022-12-06 17:49:41 +0100

    2022 - day 3 - part 1

---
+++ 2022/03/1.js
@@ -0,0 +1,51 @@
+import { AdventOfCode as BaseAdventOfCode } from '../../AdventOfCode.js'
+
+class AdventOfCode extends BaseAdventOfCode
+{
+  constructor(inputFileName) {
+    super(inputFileName)
+  }
+
+  splitInHalf(string) {
+    return [string.slice(0, Math.floor(string.length / 2)), string.slice(Math.floor(string.length / 2))]
+  }
+
+  parseInput(data) {
+    data = data.trim().split('\n').filter(value => value)
+    data = data.map((item) => this.splitInHalf(item))
+
+    return data
+  }
+
+  callback() {
+    let commonItemTypes = []
+
+    for (const [ firstCompartment, secondCompartment ] of this.input) {
+      const rucksackCommonItems = {}
+
+      for (const item of firstCompartment) {
+        if (secondCompartment.includes(item)) {
+          rucksackCommonItems[item] = true
+        }
+      }
+
+      commonItemTypes = [...commonItemTypes, ...Object.keys(rucksackCommonItems)]
+    }
+
+    commonItemTypes = commonItemTypes.map((item) => {
+      let letterCode = item.charCodeAt(0)
+
+      if (letterCode >= 65 && letterCode <= 90) {
+        letterCode = letterCode - 38
+      } else {
+        letterCode = letterCode - 96
+      }
+
+      return letterCode
+    })
+
+    return commonItemTypes.reduce((partialSum, a) => partialSum + a, 0)
+  }
+}
+
+new AdventOfCode('input').run()

38df65455cf0d9f6f715c251a0d8c4f930b913d5
Ľuboško Anguš <lubos.angus@riesenia.com>
2022-12-06 21:01:57 +0100

    2022 - day 4 - part 2

---
+++ 2022/04/2.js
@@ -0,0 +1,24 @@
+import { AdventOfCode as BaseAdventOfCode } from '../../AdventOfCode.js'
+
+class AdventOfCode extends BaseAdventOfCode
+{
+  constructor(inputFileName) {
+    super(inputFileName)
+  }
+
+  callback() {
+    let overlappingCount = 0;
+
+    for (const pair of this.input) {
+      const [ firstMin, firstMax, lastMin, lastMax ] = pair.split(/-|,/).map((item) => parseInt(item))
+
+      if (firstMin <= lastMax && lastMin <= firstMax) {
+        overlappingCount++
+      }
+    }
+
+    return overlappingCount
+  }
+}
+
+new AdventOfCode('input').run()

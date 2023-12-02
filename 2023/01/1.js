cb5bfb78839f2b631194347852017e2339a03016
Ľuboško Anguš <lubos.angus@riesenia.com>
2023-12-02 09:37:47 +0100

    2023 - day 1 - part 1

---
+++ 2023/01/1.js
@@ -0,0 +1,22 @@
+import { AdventOfCode as BaseAdventOfCode } from '../../AdventOfCode.js'
+
+class AdventOfCode extends BaseAdventOfCode
+{
+  constructor(inputFileName) {
+    super(inputFileName)
+  }
+
+  callback() {
+    const numbers = this.input.map((line) => {
+      const numbers = line.replace(/[^0-9]/g, '')
+      const first = numbers[0]
+      const last = numbers[numbers.length - 1]
+
+      return parseInt(`${first}${last}`)
+    })
+
+    return numbers.reduce((a, b) => a + b)
+  }
+}
+
+new AdventOfCode('input').run()

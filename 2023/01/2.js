e8076d6533e873282112b63c0eb09858b3f3ef0b
Ľuboško Anguš <lubos.angus@riesenia.com>
2023-12-02 10:47:24 +0100

    2023 - day 1 - part 2

---
+++ 2023/01/2.js
@@ -0,0 +1,47 @@
+import { AdventOfCode as BaseAdventOfCode } from '../../AdventOfCode.js'
+
+class AdventOfCode extends BaseAdventOfCode
+{
+  constructor(inputFileName) {
+    super(inputFileName)
+  }
+
+  callback() {
+    const numbers = this.input.map((line) => {
+      const validStringNumbers = ['-','one','two','three','four','five','six','seven','eight','nine']
+      const lineNumbers = []
+
+      for (let index = 0; index < line.length; index++) {
+        const slicedLine = line.slice(index)
+
+        if (!isNaN(parseInt(line[index]))) {
+          lineNumbers.push(line[index])
+
+          continue
+        }
+
+        for (const stringNumber of validStringNumbers) {
+          const lineMatch = slicedLine.match(`^${stringNumber}`)
+
+          if (!lineMatch) continue
+
+          lineNumbers.push(validStringNumbers.indexOf(stringNumber))
+        }
+      }
+
+      const first = lineNumbers[0]
+      const last = lineNumbers[lineNumbers.length - 1]
+
+      // console.log(line)
+      // console.log(lineNumbers)
+      // console.log(`${first}${last}`)
+      // console.log('-------------')
+
+      return parseInt(`${first}${last}`)
+    })
+
+    return numbers.reduce((a, b) => a + b)
+  }
+}
+
+new AdventOfCode('input').run()

28cb5e23e3195d1cf4fee1cd3d949dc106fa1aed
Ľuboško Anguš <lubos.angus@riesenia.com>
2023-12-03 10:24:28 +0100

    2023 - day 2 - part 2

---
+++ 2023/02/2.js
@@ -0,0 +1,59 @@
+import { AdventOfCode as BaseAdventOfCode } from '../../AdventOfCode.js'
+
+class AdventOfCode extends BaseAdventOfCode
+{
+  constructor(inputFileName) {
+    super(inputFileName)
+  }
+
+  callback() {
+    const games = []
+
+    for (const line of this.input) {
+      const regex = /Game (?<gameId>\d+): (?<game>.*)/gm;
+      const { groups: { gameId, game } } = regex.exec(line)
+
+      const maxColors = {
+        red: 0,
+        green: 0,
+        blue: 0,
+      }
+
+      const rounds = game.split('; ').map((round) => {
+        const items = round.split(', ')
+        const colors = {
+          red: 0,
+          green: 0,
+          blue: 0,
+        }
+
+        for (const item of items) {
+          const [value, color] = item.split(' ')
+
+          colors[color] = parseInt(value)
+
+          if (colors[color] > maxColors[color]) {
+            maxColors[color] = colors[color]
+          }
+        }
+
+        return colors
+      });
+
+      games.push({
+        gameId: parseInt(gameId),
+        rounds,
+        maxColors,
+      })
+    }
+
+    let result = 0;
+    for (const game of games) {
+      result += game.maxColors.red * game.maxColors.green * game.maxColors.blue
+    }
+
+    return result
+  }
+}
+
+new AdventOfCode('input').run()

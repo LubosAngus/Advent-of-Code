4800f8077badefb4439139abe38c0cf054b1d69a
anguslubos <angus.lubos@gmail.com>
2023-12-06 23:02:41 +0100

    2023 - day 4 - part 1

---
+++ 2023/04/1.js
@@ -0,0 +1,38 @@
+import { AdventOfCode as BaseAdventOfCode } from '../../AdventOfCode.js'
+
+class AdventOfCode extends BaseAdventOfCode
+{
+  constructor(inputFileName) {
+    super(inputFileName)
+  }
+
+  callback() {
+    let finalScore = 0
+
+    for (const line of this.input) {
+      const regex = /Card (?<cardId>[0-9 ]+): (?<winning>.+) \| (?<yours>.+)/gm;
+      let { groups: { cardId, winning, yours } } = regex.exec(line)
+
+      winning = winning.trim().split(' ').filter(v => v).map(Number)
+      yours = yours.trim().split(' ').filter(v => v).map(Number)
+
+      const yourWinning = []
+      for (const y of yours) {
+        if (winning.includes(y)) {
+          yourWinning.push(y)
+        }
+      }
+
+      let tmpScore = 0
+      for (let index = 0; index < yourWinning.length; index++) {
+        tmpScore = (tmpScore * 2) || 1
+      }
+
+      finalScore += tmpScore
+    }
+
+    return finalScore
+  }
+}
+
+new AdventOfCode('input').run()

099b5b6a3591ba380e3bbae7aa6c28dead0b26e9
Ľuboško Anguš <lubos.angus@riesenia.com>
2023-12-09 11:05:26 +0100

    2023 - day 9 - part 1

---
+++ 2023/09/1.js
@@ -0,0 +1,60 @@
+import { AdventOfCode as BaseAdventOfCode } from '../../AdventOfCode.js'
+
+class AdventOfCode extends BaseAdventOfCode
+{
+  constructor(inputFileName) {
+    super(inputFileName)
+  }
+
+  generateSteps(value) {
+    let currentSteps = value
+    let steps = [value]
+
+
+    while (!currentSteps.every(value => value === 0)) {
+      const nextSteps = []
+
+      for (let index = 0; index < currentSteps.length - 1; index++) {
+        const nextStep = currentSteps[index + 1] - currentSteps[index]
+
+        nextSteps.push(nextStep)
+      }
+
+      steps.push(nextSteps)
+
+      currentSteps = nextSteps
+    }
+
+    return steps
+  }
+
+  generateNextValue(steps) {
+    let nextValue = 0
+
+    steps.reverse()
+
+    for (const step of steps) {
+      step.reverse()
+
+      nextValue += step[0]
+    }
+
+    return nextValue
+  }
+
+  callback() {
+    const values = this.input.map(value => value.split(' ').map(Number))
+    const nextValues = []
+
+    for (const value of values) {
+      const steps = this.generateSteps(value)
+      const nextValue = this.generateNextValue(steps)
+
+      nextValues.push(nextValue)
+    }
+
+    return nextValues.reduce((acc, value) => acc + value, 0)
+  }
+}
+
+new AdventOfCode('input').run()

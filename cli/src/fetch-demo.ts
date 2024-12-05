8544710dddf19f96fcbc3656e844f88999fe1e52
angus.lubos <angus.lubos@gmail.com>
2024-12-05 15:11:16 +0100

    choose demo, disable already existing ones

--- cli/src/fetch-demo.ts
+++ cli/src/fetch-demo.ts
@@ -1,10 +1,13 @@
 import truncateString from '@advent-utils/truncate-string'
 import { checkbox } from '@inquirer/prompts'
 import chalk from 'chalk'
+import { readdir, readFile } from 'fs/promises'
 import { JSDOM } from 'jsdom'
 import { type Ora } from 'ora'
+import path from 'path'
 import cleanupBeforeExit from './cleanup-before-exit'
 import fetchWithCookie from './fetch-with-cookie'
+import getFolderPath from './get-folder-path'
 
 export default async (loadingSpinner: Ora): Promise<string[]> => {
   const response = await fetchWithCookie(
@@ -30,14 +33,40 @@ export default async (loadingSpinner: Ora): Promise<string[]> => {
     })
 
   if (possibleDemos.length > 1) {
+    const existingDemos = new Map<string, string>()
+
+    try {
+      const folderPath = getFolderPath()
+      const files = await readdir(folderPath)
+      const demoFilesNames = files.filter((file) => {
+        return file.startsWith('demo') && file.endsWith('.txt')
+      })
+
+      const filesPromises = demoFilesNames.map((file) =>
+        readFile(path.join(folderPath, file), 'utf-8'),
+      )
+
+      const demoFiles = await Promise.all(filesPromises)
+
+      for (let i = 0; i < demoFiles.length; i++) {
+        existingDemos.set(demoFiles[i].trim(), demoFilesNames[i])
+      }
+    } catch {
+      // nevermind
+    }
+
     loadingSpinner.stop()
 
     const selectedDemos = await checkbox({
       message: 'Which demos?',
       choices: possibleDemos.map((item) => ({
         value: item,
-        name: truncateString(item.split('\n')[0], 24),
+        name: truncateString(item.split('\n')[0], 14).padEnd(15, ' '),
         description: '\n' + item,
+        checked: existingDemos.has(item),
+        disabled: existingDemos.has(item)
+          ? chalk.italic(existingDemos.get(item))
+          : false,
       })),
       required: true,
       loop: false,
@@ -50,7 +79,7 @@ export default async (loadingSpinner: Ora): Promise<string[]> => {
       process.exit(0)
     })
 
-    return selectedDemos
+    return [...existingDemos.keys(), ...selectedDemos]
   }
 
   return possibleDemos

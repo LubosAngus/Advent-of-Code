150c3940be9c0145b094d779286b07f77fb59d40
angus.lubos <angus.lubos@gmail.com>
2024-12-05 11:23:13 +0100

    solutions template update

--- solutions/template.ts
+++ solutions/template.ts
@@ -1,7 +1,9 @@
 import { readFileSync } from 'fs'
 
-const rawData = readFileSync('./demo.txt', 'utf-8')
-// const rawData = readFileSync('./input.txt', 'utf-8')
+// const inputName = 'demo1'
+const inputName = 'input'
+
+const rawData = readFileSync(`./${inputName}.txt`, 'utf-8')
 
 function parseInput(data: string) {
   return data.split('\n').map((row) => row.split(' ').map(Number))
